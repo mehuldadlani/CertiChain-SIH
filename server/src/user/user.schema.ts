c8633572c11a435453031ac132d05ae7d74a34c4
UtkarshRastogi0712 <utkarshrastogi0712@gmail.com>
2023-09-12 02:29:14 +0530

    User Schema setup

---
+++ server/src/user/user.schema.ts
@@ -0,0 +1,32 @@
+import mongoose, { Schema } from "mongoose";
+
+const userSchema: Schema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  walletAddress: {
+    type: String,
+    required: true,
+  },
+  dob: {
+    type: Date,
+    required: true,
+  },
+  aadharNumber: {
+    type: String,
+    required: true,
+  },
+  certificates: [
+    {
+      type: String,
+      required: false,
+    },
+  ],
+});
+
+export default mongoose.model("User", userSchema);
