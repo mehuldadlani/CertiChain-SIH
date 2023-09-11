1856d08f134483bf5c5d85ef157b95bc413e5372
Ankit Choudhary <sign.ankit14@gmail.com>
2023-09-12 03:18:40 +0530

    Added SignUp page

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,7 +1,18 @@
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+	content: ["./src/**/*.{js,jsx,ts,tsx}"],
+	theme: {
+		extend: {
+			colors: {
+				signUpBg: "#f2f2f2",
+			},
+			backgroundImage: {
+				signInGradient:
+					"linear-gradient(90deg, #F250DA 4.03%, #874DEA 23.61%, #A84EE6 96.13%)",
+			},
+			boxShadow: {
+				xs: "0px 4px 4px 0px rgba(0, 0, 0, 0.25)",
+			},
+		},
+	},
+	plugins: [],
 };
