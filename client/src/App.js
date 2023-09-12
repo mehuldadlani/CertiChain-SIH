c26537d3b2918de73110d71f437ec0489f5a1d4e
Yashaswini Singh Shaktawat <yashaswinisinghshaktawat@gmail.com>
2023-09-12 13:30:10 +0530

    Added Navbar

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,8 @@ import "@particle-network/connect-react-ui/dist/index.css";
 import UserReg from "./components/Forms/UserReg";
 import LoginChoice from "./components/Auth/LoginChoice";
 import OrgReg from "./components/Forms/OrgReg";
+import UserDashboard from "./components/Dashboard/UserDashboard";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
@@ -14,6 +16,8 @@ function App() {
         <Route path="/user/reg" element={<UserReg />} />
         <Route path="/mode" element={<LoginChoice />} />
         <Route path="/org/reg" element={<OrgReg />} />
+        <Route path="/user/dashboard" element={<UserDashboard />} />
+        <Route path="Navbar" element={<Navbar />} />
       </Routes>
     </div>
   );
