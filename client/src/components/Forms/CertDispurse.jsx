63a42f6844f941444a659e1291185587962d187b
Ankit Choudhary <sign.ankit14@gmail.com>
2023-09-13 01:48:40 +0530

    Minor UI tweaks

--- client/src/components/Forms/CertDispurse.jsx
+++ client/src/components/Forms/CertDispurse.jsx
@@ -114,9 +114,43 @@ export default function CertDispurse() {
 		}
 	};
 
-	const generateCertificate = () => {
+	const generateCertificate = async () => {
 		// This is assuming that rendering CertificateCanvas somewhere sets the ticketUrl
 		// If this is not the case, adjust the logic accordingly
+		// function makeStorageClient() {
+		// 	return new Web3Storage({ token: process.env.REACT_APP_ACCESS_TOKEN });
+		// }
+
+		// async function makeFileObjects(data) {
+		// 	const blob = new Blob([JSON.stringify(data)], {
+		// 		type: "application/json",
+		// 	});
+
+		// 	const files = [new File([blob], "nftInfo.json")];
+		// 	console.log(files);
+		// 	return files;
+		// }
+
+		// async function storeFiles(data) {
+		//     console.log("Uploading data to IPFS with web3.storage....");
+
+		//     const files = await makeFileObjects(data);
+		//     const client = makeStorageClient();
+		//     const cid = await client.put(files, { wrapWithDirectory: false });
+
+		//     console.log(files, client, cid)
+
+		//     return cid;
+		// }
+
+		// const storeImage = async (files) => {
+		// 	console.log("Uploading image to IPFS with web3.storage....");
+		// 	const client = makeStorageClient();
+		// 	const cid = await client.put([files], { wrapWithDirectory: false });
+		// 	console.log("Stored Image with cid:", cid);
+		// 	return cid;
+		// };
+
 		setCertificateDetails({
 			name: name,
 			logo: logo,
