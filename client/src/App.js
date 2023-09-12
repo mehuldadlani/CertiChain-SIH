f84c1520695244b7dadea217797ee7c24b068574
Yashaswini Singh Shaktawat <yashaswinisinghshaktawat@gmail.com>
2023-09-12 09:04:20 +0530

    Added Router

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,14 @@
 import { ConnectButton } from "@particle-network/connect-react-ui";
 import SignUp from "./components/SignUp";
+import { Routes, Route } from "react-router-dom"
 import "@particle-network/connect-react-ui/dist/index.css";
 
 function App() {
   return (
     <div>
-      <SignUp />
+       <Routes>
+        <Route path="/" element={ <SignUp/> } />
+        </Routes>
     </div>
   );
 }
