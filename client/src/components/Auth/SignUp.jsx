6a246ec671bab0f639b0317c0e68bf32de4f7f7b
Ankit Choudhary <sign.ankit14@gmail.com>
2023-09-12 12:28:21 +0530

    Updated Contract address and minor changes in Sign Up page

--- client/src/components/Auth/SignUp.jsx
+++ client/src/components/Auth/SignUp.jsx
@@ -15,13 +15,16 @@ export default function SignUp() {
 				<img className="w-[189px] h-5 shrink-0" src={logo} />
 			</div>
 			<div className="shrink-0 flex justify-center items-center mt-20">
-				<div className="w-[309px] h-max py	-6 shrink-0 rounded-3xl bg-signUpBg shadow-xs flex flex-col justify-center items-center xl:w-[479px] xl:h-[262px]">
+				<div className="w-[309px] h-max py-6 shrink-0 rounded-3xl bg-signUpBg shadow-xs flex flex-col justify-center items-center xl:w-[479px] xl:h-[262px]">
 					<div className="w-[140px] h-[209px] shrink-0 flex flex-col items-center justify-between">
 						<div className="shrink-0 text-center text-black Inder font-semibold text-xl leading-5">
 							Sign Up
 						</div>
-						<button className="w-[180px] h-11 shrink-0 rounded-3xl bg-signInGradient flex items-center justify-center">
-							<img className="w-9 h-8 shrink-0 opacity-90" src={signInLogo} />
+						<button className="flex items-center justify-center w-[180px] h-11 shrink-0 rounded-3xl bg-signInGradient">
+							<img
+								className="w-12 h-12 shrink-0 opacity-90 mt-4"
+								src={signInLogo}
+							/>
 							<div className="text-[#FFF] Inder text-sm font-normal">
 								<ConnectButton.Custom>
 									{({ account, openConnectModal }) => {
